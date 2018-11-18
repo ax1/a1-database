cfb103332a879695b06d87244047686ca230ef22
Angel Rego <angelrego@gmail.com>
2018-11-18 15:12:03 +0100

    test: add color to output

--- test/test.js
+++ test/test.js
@@ -40,3 +40,5 @@ async function test() {
 }
 
 test()
+  .then(() => console.log('\x1b[32m%s\x1b[0m', '✔ TESTS OK'))
+  .catch(err => { console.log('\x1b[31m%s\x1b[0m', '✘ TESTS NOT OK'); console.error(err) })

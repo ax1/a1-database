7ed174f74bd3bf177073824f3494c5a4bf6b613a
Angel Rego <angelrego@gmail.com>
2020-08-20 00:39:58 +0200

    test: make exhaustive tests
    17 assertions for a 200 lines database.js code

--- test/test.js
+++ test/test.js
@@ -6,43 +6,25 @@ const DATABASE_PATH = __dirname + '/test.db'
 const item = { name: 'juan', age: 31 }
 const item2 = { id: 'juan', age: 31 }
 let db
+
 async function test() {
   try {
     // connect to database (file location is process.pwd() + file)
     db = await database.connect(DATABASE_PATH)
-    await db.delete(() => true)
-    // the elements are just JSON objects (plain string files also accepted if only for reading and searching. eg: log files)
+    await db.delete(() => true) //clean db
+
+    // perform operations
     await testInsert()
     await testUpsert()
     await testUpdate()
-    // function to filter data
-    const filter = el => el.name === item.name
-    // find
-    let results = await db.find(filter)
-    assert.equal(results.length, 0, `find returned ${results.length} values`)
-    // store object
-    for (let r = 0; r < 10000; r++) {
-      item.length = r
-      await db.save(item)
-    }
-    let count = await db.save(item)
-    assert.equal(count, 1, `save 1 item`)
-    const items = [item, item, item]
-    count = await db.save(items)
-    assert.equal(count > 1, true, `save several items`)
-    // this time, save but also delete old objects
-    item.age = 33
-    count = await db.save(item, filter) // this "delete" takes long time because of 1-splice and 2-persist delete rows. For the test it could be hugely improved both parts but in the real life you don't usually delete many rows
-    assert.equal(count < 0, true, `save 1 item while deleting the old elements with the same 'name'`)
-    // delete all the elements in the db
-    await db.delete(() => true)
-    count = (await db.find(() => true)).length
-    assert.deepStrictEqual(count, 0, `database clean`)
+    await testSave()
+    await testFind()
+    await testLogLines()
+    await testDelete()
+
     // disconnect is optional
     await database.disconnect(db)
-    console.log('database tests passed!')
   } catch (err) {
-    //assert.fail(err.toString()) //assert.fail exits the function, so the rejected promise is not fulfilled
     console.error(err)
   }
 }
@@ -63,6 +45,33 @@ async function testUpdate() {
   assert(await db.update(item2) === 0, 'Update should replace a duplicated id')
   const newItem = { id: 'pepe' }
   assert.rejects(async () => await db.update(newItem), 'Update should throw if a new element')
+}
+
+async function testDelete() {
+  // delete 1 element
+  assert(await db.delete(el => el.id === item2.id) > 0, 'Delete should remove items')
+  // delete all the elements in the db
+  await db.delete(() => true)
+  const count = (await db.find(() => true)).length
+  assert.deepStrictEqual(count, 0, `database clean`)
+}
+
+async function testSave() {
+  assert(await db.save(item) > 0, 'save should add non-id items')
+  assert(await db.save(item2) === 0, 'Upsert should replace a duplicated id')
+}
+
+async function testFind() {
+  const filter = el => el.name === item.name
+  let results = await db.find(filter)
+  assert(results.length > 0, `find returned ${results.length} values`)
+}
+
+async function testLogLines() {
+  // plain string files also accepted if only for reading and searching. eg: log files
+  assert(await db.save('this is a log line'), 'should allow for plain log files')
+  const results = await db.find(() => true)
+  assert(results.length > 0, `should find and retrieve log files`)
 }
 
 async function testLoad() {
